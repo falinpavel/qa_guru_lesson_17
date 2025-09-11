ae645aebf41f2eb119915e725332b224cbfeb36a
Falin Pavel <falinpavel96@gmail.com>
2025-09-11 23:02:24 +0300

    remove schemas

--- utils/path_helpers.py
+++ utils/path_helpers.py
@@ -4,4 +4,4 @@ from pathlib import Path
 
 def get_schema_path(schema_name):
     base_directory = Path(__file__).resolve().parent.parent
-    return os.path.join(base_directory, 'tests', 'schemas', schema_name)
+    return os.path.join(base_directory, 'resources', 'schemas', schema_name)
